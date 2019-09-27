fcc7b1154b4c3f59702f1cc01fc33b3ecde71eb9
Christopher Fornesa <admin@cftxp.net>
2019-09-27 17:06:55 -0500

    Update the Footer
    Update the footer to reflect the changes in the file path for the main portfolio as well as to reflect the changes in the file path for the PHP version of this website.

--- src/Footer.js
+++ src/Footer.js
@@ -8,15 +8,15 @@ class Footer extends React.Component {
     return (
       <footer>
         Created &amp; Designed by{" "}
-        <a href="https://chrisfornesa.com" className="App-link">
+        <a href="https://www.chrisfornesa.com" className="App-link">
           Chris Fornesa
         </a>{" "}
         and{" "}
         <a href="https://wokeus.com" className="App-link">
           Woke Us Designs
         </a>{" "}
-        &copy; <Year />. |{" "}
-        <a href="https://php.privilege.wokeus.com" className="App-link">
+        &copy; <Year />. ||{" "}
+        <a href="https://privilege.php.chrisfornesa.com" className="App-link">
           This site in PHP
         </a>
         .
